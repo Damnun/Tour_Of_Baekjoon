f14cef12bb13d05a8b15f8cd2367e1a5a8bdd06c
이재헌 <shmom467@hanmail.net>
2021-01-08 14:34:19 +0000

    Baekjoon No.10869 grading No.25066723 28776KB, 68ms, 179B

---
+++ index.py
@@ -0,0 +1,13 @@
+while True:
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    if a >= 1 and b <= 10000 : break
+        
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(int(a%b))
+
+ #(0 < A, B < 10)`
