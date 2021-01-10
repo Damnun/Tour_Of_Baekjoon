a62d1a7937d7a94eea67cdce2313a7cd8f234b0d
이재헌 <shmom467@hanmail.net>
2021-01-10 09:51:19 +0000

    Baekjoon No.2753 윤년

--- index.py
+++ index.py
@@ -1,24 +1,15 @@
-grade = input()
-grade = int(grade)
+#윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+# 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다. 1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다. 하지만, 2000년은 400의 배수이기 때문에 윤년이다.
 
-while True:
-    if grade < 0 or grade > 100:
-        grade = input()
-        grade = int(grade)
+year = input()
+year = int(year)
+
+if year % 4 == 0:
+    if year % 100 != 0 or year % 400 == 0:
+        print('1')
     else:
-        break
-
-if grade >= 90:
-    print('A')
-
-elif grade < 90 and grade >= 80:
-    print('B')
-
-elif grade < 80 and grade >= 70:
-    print('C')
-
-elif grade < 70 and grade >= 60:
-    print('D')
+        print('0')
 
 else:
-    print('F')
+    print('0')
+
