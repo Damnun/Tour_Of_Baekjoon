a19608156d8b18ac55d44087120cc2ffaa3232e9
이재헌 <shmom467@hanmail.net>
2021-01-11 14:46:54 +0000

    Baekjoon No.2439 Star - 2

--- index.py
+++ index.py
@@ -1,2 +1,8 @@
-for n in range(int(input())):
-    print('*'*(n+1))
+a = int(input())
+
+for i in range(a):
+    if i != 0:
+        print("")
+    for j in range(a-(i+1)):
+        print(" ", end='')
+    print("*"*(i+1), end='')
