c802c193d65ac2e1404de9efab30217c2646ec9a
이재헌 <shmom467@hanmail.net>
2021-01-11 14:11:26 +0000

    Baekjoon No.8393

--- index.py
+++ index.py
@@ -1,7 +1,21 @@
-index = int(input())
+while True:
+    h, m = input().split()
+    h = int(h)
+    m = int(m)
 
-for i in range(index):
-    a, b = input().split()
-    a = int(a)
-    b = int(b)
-    print(a+b)
+    if h >= 0 and h <= 23:
+        break
+    if m >= 0 and h <= 59:
+        break
+
+if m >= 45:
+    m -= 45
+
+else:
+    if h-1 < 0:
+        h = 23
+    else:
+        h -= 1
+    m += 15
+
+print(h, m)
