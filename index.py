cf22f558eac3111e1e22075281861360b4c08516
이재헌 <shmom467@hanmail.net>
2021-01-11 14:27:30 +0000

    Baekjoon No.2741

--- index.py
+++ index.py
@@ -1,10 +1,5 @@
-while True:
-    a = int(input())
-    if 1 <= a and a <= 10000:
-        break
+import sys
 
-result = 0
-for i in range(a):
-    result += (i+1)
-
-print(result)
+for index in range(int(sys.stdin.readline().rstrip())):
+    b ,c = map(int,sys.stdin.readline().split())
+    print(int(b)+int(c))
