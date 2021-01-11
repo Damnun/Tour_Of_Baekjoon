1f2280a9ebb57de7fd92d5f573a9e036ca1ebeb0
이재헌 <shmom467@hanmail.net>
2021-01-11 03:19:07 +0000

    Baekjoon NO.2739 Multiple / use For-loop

--- index.py
+++ index.py
@@ -1,21 +1,5 @@
-while True:
-    h, m = input().split()
-    h = int(h)
-    m = int(m)
+num = input()
+num = int(num)
 
-    if h >= 0 and h <= 23:
-        break
-    if m >= 0 and h <= 59:
-        break
-
-if m >= 45:
-    m -= 45
-
-else:
-    if h-1 < 0:
-        h = 23
-    else:
-        h -= 1
-    m += 15
-
-print(h, m)
+for i in range(1, 10):
+     print(f"{num} * {i} = {num*i}")
