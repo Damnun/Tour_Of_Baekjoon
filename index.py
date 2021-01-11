0b2c60f7c5a4670bb04bf542909baf08562a3f2c
이재헌 <shmom467@hanmail.net>
2021-01-11 14:35:39 +0000

    Baekjoon No.2438

--- index.py
+++ index.py
@@ -1,7 +1,2 @@
-a = int(input())
-for i in range(a):
-    b, c = input().split()
-    b = int(b)
-    c = int(c)
-    result = b + c
-    print("Case #%d: %d + %d = %d" %(i+1, b, c, result))
+for n in range(int(input())):
+    print('*'*(n+1))
