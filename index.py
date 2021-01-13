feacc0fc35e7125f5bd695a5f160a1b7c29a00e2
이재헌 <shmom467@hanmail.net>
2021-01-13 13:47:10 +0000

    Baekjoon No.1110 더하기 사이클 어렵진 않은데 졸려서 그런지 계산이 잘 안된듯. 좀 더 깔끔하게 짤 수 있을 것 같은데..

--- index.py
+++ index.py
@@ -1,6 +1,17 @@
+def cal(x):
+    if x >= 0 and x < 10:
+        x *= 10
+    x = (x / 10) + (x % 10)
+    return int(x)
+
+
+tmp = index = num = int(input())
+cnt = 0
+
 while True:
-    try:
-        a, b = map(int, input().split())
-        print(a+b)
-    except EOFError:
+    num = cal(tmp)
+    tmp = ((tmp % 10) * 10) + (num % 10)
+    cnt += 1
+    if tmp == index:
+        print(cnt)
         break
