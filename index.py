126a906705fa6a3dc2e62cf385b5990bf7a629f6
이재헌 <shmom467@hanmail.net>
2021-01-13 13:11:06 +0000

    Baekjoon No.10951 EOF >> End of File try-except 를 사용하여 예상되는 오류를 캐치할 수 있다. 자주 사용하려나?

--- index.py
+++ index.py
@@ -1,10 +1,6 @@
-a, b = map(int, input().split())
-c = input().split()
-
-
-for i in range(a):
-    if int(c[i]) < b:
-        print(int(c[i]), end=' ')
-
-num = 3
-print(f"{num}", num)
+while True:
+    try:
+        a, b = map(int, input().split())
+        print(a+b)
+    except EOFError:
+        break
