f525a7aa1d84535d6579dcef1a4e13afdf567dc8
이재헌 <shmom467@hanmail.net>
2021-01-20 13:54:40 +0000

    벌집의 이동경로 계산하기 규칙성을 파악하면  정의를 내리기 쉬운 문제였음.

--- Baekjoon_No2292.py
+++ Baekjoon_No2292.py
@@ -2,8 +2,13 @@ a = int(input())
 index = 1
 stack = 1
 
-while (a - stack) > 0:
-    print("stack : ", stack)
-    stack += 6
-    index += 1
-print(stack/6)
+if a == 1:
+    print(1)
+
+else:
+    while True:
+        stack += (6*index)
+        index += 1
+        if a <= stack:
+            print(index)
+            break
