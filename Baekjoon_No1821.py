7c889886f631cb9665119345a7e0406408ad2ad0
이재헌 <shmom467@hanmail.net>
2021-01-23 08:14:45 +0000

    자연수의분할 P(n,k)를 입력받은 수 가 될때 까지 순환으로 카운트 해서 받고싶은데, 분할의 경우의 수를 구하는게 어려워서 막힌상태이다.

--- Baekjoon_No1821.py
+++ Baekjoon_No1821.py
@@ -4,3 +4,13 @@ a.append(n-(k-1))
 for i in range(k-1):
     a.append(1)
 print(a)
+
+index = 0
+while True:
+    if a[0] < a[1]:
+        break
+    elif a[index] > a[index+1] and a[index+1]+1 > a[index+2]:
+        a[index] -= 1
+        a[index+1] += 1
+    print(a, index)
+    index += 1
