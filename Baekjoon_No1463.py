8ed439184a520a9a513ea8761f5fa56aff87c506
이재헌 <shmom467@hanmail.net>
2021-01-27 12:48:07 +0000

    .

--- Baekjoon_No1463.py
+++ Baekjoon_No1463.py
@@ -1,13 +1,6 @@
 a = int(input())
 count = 0
 while a != 0:
-    if (a-1) / 9 >= 1 and (a-1) % 9 == 0:
-        a -= 1
-        count += 1
-
-    elif (a-1) / 4 >= 1 and (a-1) % 4 == 0:
-        a -= 1
-        count += 1
 
     elif a % 3 == 0:
         a /= 3
