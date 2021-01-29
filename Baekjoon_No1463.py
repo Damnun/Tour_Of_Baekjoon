05b413093385395c3bcababbd77d437435d27610
이재헌 <shmom467@hanmail.net>
2021-01-29 12:51:57 +0000

    ..

--- Baekjoon_No1463.py
+++ Baekjoon_No1463.py
@@ -2,16 +2,16 @@ a = int(input())
 count = 0
 
 while a != 0:
-    if a / 9 >= 1 or a / 4 >= 1:
+    if a // 9 >= 1 or a // 4 >= 1:
         a -= 1
         count += 1
 
     elif a % 3 == 0:
-        a /= 3
+        a //= 3
         count += 1
 
     elif a % 2 == 0:
-        a /= 2
+        a //= 2
         count += 1
 
     else:
