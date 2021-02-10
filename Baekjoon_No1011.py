4d4b9f3878b5a1751c9b0029b539dae99cb09d15
Universe_lee <72370701+Damnun@users.noreply.github.com>
2021-02-10 12:25:32 +0900

    Fly me to the Alpha Centauri
    시간초과를 해결하기.

--- Baekjoon_No1011.py
+++ Baekjoon_No1011.py
@@ -1,48 +1,80 @@
-a, b = map(int, input().split())
-cnt = 0
-tmp = 0
-c = [-1, 0, 1]
+import math
+ans = int(input())
+for i in range(ans):
+    arr = [1]
+    tmp = 2
+    a, b = map(int, input().split())
+    if (b-a) <= 3:
+        print(b-a)
+    else:
+        n = int(math.sqrt(b-a))
+        if b-a == n ** 2:
+            print(2*n-1)
+        elif n ** 2 < b-a <= n ** 2 + n:
+            print(2*n)
+        else:
+            print(2*n+1)
 
-while a != b:
-    print(a, b)
-    print(c[::-1], tmp)
-    for i in c[::-1]:
-        if a + (i + tmp) == b:
-            cnt += 1
-            a += (i + tmp)
+
+
+
+
+
+"""
+f(n-1) < n <= f(n)
+		    n	차	f(n)
+1		    1	0	1
+11		    2	1	2
+111         3   1   3
+121		    4	2	4
+1221		6	2	5
+12321		9	3	6
+123321		12	3	7
+1234321		16	4	8
+12344321	20	4	9
+123454321	25	5	10
+1234554321	30	5	11
+12345654321	36	6	12
+
+ans = int(input())
+for i in range(ans):
+    arr = [1]
+    tmp = 2
+    lresult = rresult = 0
+    a, b = map(int, input().split())
+    while True:
+        arr2 = arr+[]
+        arr2.reverse()
+        lresult = (sum(arr) * 2 - arr2[0])
+        rresult = sum(arr) * 2
+        if lresult >= (b-a):
+            print(len(arr)*2-1)
             break
 
-        elif a + (i + tmp) <= b:
-            cnt += 1
-            a += (i + tmp)
-            break
-    tmp += 1
-
-print(cnt)
-
-
-
-""""a, b = map(int, input().split())
-b = b - a
-a = 0
-cnt = 1
-tmp = 0
-c = [0, 1, 2]
-
-while b > 0:
-    print(a, b)
-    print(c[::-1], tmp)
-    for i in c[::-1]:
-        if b - (i + tmp) == 0:
-            cnt += 1
-            b -= (i + tmp)
+        elif rresult >= (b-a):
+            print(len(arr)*2)
             break
 
-        elif b - (i + tmp) >= 0:
-            cnt += 1
-            b -= (i + tmp)
-            break
-    tmp += 1
+        arr.append(tmp)
+        tmp += 1
 
-print(cnt)
+
+
+
+f(n-1) < n <= f(n)
+		    n	차	f(n)
+1		    1	0	1
+11		    2	1	2
+121		    4	2	3
+1221		6	2	4
+12321		9	3	5
+123321		12	3	6
+1234321		16	4	7
+12344321	20	4	8
+123454321	25	5	9
+1234554321	30	5	10
+12345654321	36	6	11
+"""
+
+
 """
