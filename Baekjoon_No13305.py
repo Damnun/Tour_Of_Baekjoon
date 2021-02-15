ccd185fa521c189e2a518ccdacec7e1a7b66efb0
이재헌 <shmom467@hanmail.net>
2021-02-15 13:58:36 +0000

    일직선상의 거리 이동시에 최소 기름 값을 사용하는 코드 최저 기름 값을 따로 min_price에 저장해두었다가 min_price보다 작은 값이 나오게 된다면 그 값을 min_price로 바꾸고 이동 거리와 가격을 곱해줌, 만약 min_price보다 큰 값이 나온다면 min_price와 이동 거리를 곱해주면 됨.

--- Baekjoon_No13305.py
+++ Baekjoon_No13305.py
@@ -5,16 +5,12 @@ oil_price = list(map(int, input().split()))
 result = 0
 
 del(oil_price[len(oil_price)-1])
-
-min_price = min(oil_price)
-min_index = oil_price.index(min_price)
-
-if min_index == 0:
-    result = min_price * road_length_sum
-
-else:
-    for i in range(min_index):
-        result += road_length[i] * oil_price[i]
-    result += (sum(road_length[min_index:]) * min_price)
-
+min_price = oil_price[0]
+result = oil_price[0] * road_length[0]
+for i in range(1, amount-1):
+    if oil_price[i] > min_price:
+        result += min_price * road_length[i]
+    else:
+        result += oil_price[i] * road_length[i]
+        min_price = oil_price[i]
 print(result)
