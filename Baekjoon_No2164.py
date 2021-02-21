5ee026a007d80df7457bc7a5fe28742b75b57b05
이재헌 <shmom467@hanmail.net>
2021-02-21 14:23:22 +0000

    collections의 deque를 구현하였음 ** 추가 공부 필요함 **

---
+++ Baekjoon_No2164.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+data_count = int(input())
+data = deque()
+
+for i in range(data_count):
+    data.append(i+1)
+
+while True:
+    print(data)
+    if len(data) == 1:
+        break
+    data.popleft()
+    data.append(data.popleft())
+print(data[0])
+    
