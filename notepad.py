83a06acc371aa73e6b4e3d6ee5f8cdadb93aaa2d
이재헌 <shmom467@hanmail.net>
2021-02-25 03:19:01 +0000

    .

--- notepad.py
+++ notepad.py
@@ -1,7 +1,9 @@
-n, t, c, p = map(int,input().split(" "))
-index = t
-cost = 0
-while index < n:
-    cost += c * p
-    index += t
-print(cost)
+def solution(n):
+    answer = 0
+    tmp = str(n)
+    print(tmp[::-1])
+    for i in tmp[::-1]:
+        answer += int(i) * (3**int(i))
+    return answer
+
+print(solution('125'))
