f5b3f04b4e5e9eb22a3919045dcc2c4a38983359
이재헌 <shmom467@hanmail.net>
2021-03-01 14:20:48 +0000

    BFS를 이용한 미로탈출문제, 처음 배우는 알고리즘, 역시 어렵다.

--- Baekjoon_No2178.py
+++ Baekjoon_No2178.py
@@ -3,18 +3,19 @@ from collections import deque
 n, m = map(int, input().split())
 graph = []
 for i in range(n):
-    graph.append(list(map(int, input())))
+    graph.append(list(map(int, input()))) # 맵 정보를 graph 리스트에 append
 
+# 가능한 이동경로를 dx,dy에 저장 (좌 : 0, 우 : 1, 하 : 2, 상 : 3)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs(x, y):
     queue = deque()
-    queue.append((x, y))
+    queue.append((x, y)) # Queue를 이용한 BFS 구현
     
-    while queue:
+    while queue: # Queue가 빌 때 까지
         x, y = queue.popleft()
-        for i in range(4):
+        for i in range(4): # 상하좌우 4가지 경우의 수를 전부 돌림
             nx = x + dx[i]
             ny = y + dy[i]
             
@@ -25,6 +26,7 @@ def bfs(x, y):
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
+
     return graph[n - 1][m - 1]
 
 print(bfs(0, 0))
