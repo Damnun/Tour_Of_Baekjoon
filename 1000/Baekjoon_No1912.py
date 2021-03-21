4d9d2d78f59bc6cf8f1a969b8758326bd179f92a
이재헌 <shmom467@hanmail.net>
2021-03-21 13:54:39 +0000

    DP를 처음 배워보았다. 문제에 주어진 내용에 대한 점화식이랄까.. 식의 관계를 도출해내는게 어려운 것 같다.
    주어진 데이터를 왼쪽에서부터 더해나가며
    DP테이블에 저장해주었고, 현재 값과 테이블의 값을 비교하여 더 큰 값을 매칭하여 주었다.

--- 1000/Baekjoon_No1912.py
+++ 1000/Baekjoon_No1912.py
@@ -1,5 +1,13 @@
-data_count = int(input())
+"""
+10
+10 -4 3 1 5 6 -35 12 21 -1
+"""
+loop = int(input())
 data = list(map(int, input().split()))
-max_data = max(data)
-data.remove(max_data)
-print(max_data + max(data))
+ans = [0 for _ in range(loop)]
+result = -1001
+
+for i in range(loop):
+    ans[i] = max(ans[i-1] + data[i], data[i])
+    result = max(result, ans[i])
+print(result)
