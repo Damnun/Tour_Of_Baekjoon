b799fb4f0d99a736a9ee0d0989cf59f0dac085e4
이재헌 <shmom467@hanmail.net>
2021-03-24 13:42:34 +0000

    DP로 피보나치 수열의 0과 1 출력 포인트를 찾아서 카운트 해주는 프로그램인데 그냥 수열의 일반항을 찾아서 풀어버렸다. 나중에 dp가 익숙해지면 정론으로 다시 풀어봐야겠다.

--- 1000/Baekjoon_No1003.py
+++ 1000/Baekjoon_No1003.py
@@ -7,3 +7,7 @@ for _ in range(int(input())):
             result.append([result[i][1], result[i][0] + result[i][1]])
 
     print(result[data][0], result[data][1])
+
+"""
+DP로 피보나치 수열의 0과 1 출력 포인트를 찾아서 카운트 해주는 프로그램인데 그냥 수열의 일반항을 찾아서 풀어버렸다. 나중에 dp가 익숙해지면 정론으로 다시 풀어봐야겠다.
+"""
