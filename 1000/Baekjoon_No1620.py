a92189b7263d2d1290ec7d59162119696d7ef546
이재헌 <shmom467@hanmail.net>
2021-03-25 13:39:32 +0000

    딕셔너리 자료형을 이용해 푼 문자열 구현문제로 딕셔너리를 뒤집어서 key-value와 value-key를 가질 수 있다는걸  알았다.

--- 1000/Baekjoon_No1620.py
+++ 1000/Baekjoon_No1620.py
@@ -11,3 +11,8 @@ for _ in range(m):
         print(reverse_data[int(search)])
     else:
         print(data[search])
+
+"""
+딕셔너리 자료형을 이용해 푼 문자열 구현문제로
+딕셔너리를 뒤집어서 key-value와 value-key를 가질 수 있다는걸  알았다.
+"""
