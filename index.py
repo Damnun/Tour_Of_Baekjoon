6336b068b278b731e6b59c34cadc27dc6cc58461
이재헌 <shmom467@hanmail.net>
2021-03-31 12:56:46 +0000

    programmers 문제풀이 1

--- index.py
+++ index.py
@@ -1,2 +1,32 @@
-lista = input()
-listb = [1, 2, 3, 4]
+def solution(n):
+    tmp = n + 1
+    count = 0
+    n_count = 0
+    answer = 0
+
+    while n > 0:
+        if n % 2 == 1:
+            n_count += 1
+        n //= 2
+
+    while True:
+        if count == n_count:
+            answer = tmp - 1
+            break
+        count = 0
+
+        k = tmp
+        while k > 0:
+            if k % 2 == 1:
+                count += 1
+            k //= 2
+
+        tmp += 1
+    
+    return answer
+
+print(solution(78))
+
+"""
+programmers 문제풀이 1
+"""
