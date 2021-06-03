79cfeb2e576144f5b761fb22d7024b469b91257f
이재헌 <shmom467@hanmail.net>
2021-06-03 13:54:00 +0000

    삼성 expert academy 문제를 풀어보았다.

--- index.py
+++ index.py
@@ -1,32 +1,12 @@
-def solution(n):
-    tmp = n + 1
+case = int(input())
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for n in range(case):
+    data = int(input())
     count = 0
-    n_count = 0
-    answer = 0
-
-    while n > 0:
-        if n % 2 == 1:
-            n_count += 1
-        n //= 2
-
-    while True:
-        if count == n_count:
-            answer = tmp - 1
-            break
-        count = 0
-
-        k = tmp
-        while k > 0:
-            if k % 2 == 1:
-                count += 1
-            k //= 2
-
-        tmp += 1
-    
-    return answer
-
-print(solution(78))
-
-"""
-programmers 문제풀이 1
-"""
+    for i in numbers:
+        if data % i == 0 and data // i in numbers and data <= 81:
+            count = 1
+    if count == 1:
+        print(f'#{n+1} Yes')    
+    else:
+        print(f'#{n+1} No')
