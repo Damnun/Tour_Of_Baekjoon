0eced6b57a6564701993e72034b3d30e48a58a21
이재헌 <shmom467@hanmail.net>
2021-06-20 04:50:01 +0000

    오류수정 1차

--- Baekjoon_No5373.py
+++ Baekjoon_No5373.py
@@ -1,26 +1,24 @@
 test_case = int(input())
-
-# init
 colors = ['w', 'y', 'r', 'o', 'g', 'b']
-cubes = [[0 for _ in range(9)] for _ in range(6)]
-
-count = 0
-for i in colors:
-    for j in range(9):
-        cubes[count][j] = i
-    count += 1
-
-loop_case = int(input())
-data = list(map(str, input().split()))
 
 
-def solution(loop_case, data):
+def solution(data):
     for i in data:
+        tmp_cubes = cubes
         tmp = []
         if i == 'U+':
             tmp.append(cubes[2][0])
             tmp.append(cubes[2][1])
             tmp.append(cubes[2][2])
+            cubes[0][0] = tmp_cubes[0][6]
+            cubes[0][1] = tmp_cubes[0][3]
+            cubes[0][2] = tmp_cubes[0][0]
+            cubes[0][3] = tmp_cubes[0][7]
+            cubes[0][4] = tmp_cubes[0][4]
+            cubes[0][5] = tmp_cubes[0][1]
+            cubes[0][6] = tmp_cubes[0][8]
+            cubes[0][7] = tmp_cubes[0][5]
+            cubes[0][8] = tmp_cubes[0][2]
             cubes[2][0] = cubes[5][0]
             cubes[2][1] = cubes[5][1]
             cubes[2][2] = cubes[5][2]
@@ -37,6 +35,15 @@ def solution(loop_case, data):
             tmp.append(cubes[2][0])
             tmp.append(cubes[2][1])
             tmp.append(cubes[2][2])
+            cubes[0][0] = tmp_cubes[0][2]
+            cubes[0][1] = tmp_cubes[0][5]
+            cubes[0][2] = tmp_cubes[0][8]
+            cubes[0][3] = tmp_cubes[0][1]
+            cubes[0][4] = tmp_cubes[0][4]
+            cubes[0][5] = tmp_cubes[0][7]
+            cubes[0][6] = tmp_cubes[0][0]
+            cubes[0][7] = tmp_cubes[0][3]
+            cubes[0][8] = tmp_cubes[0][6]
             cubes[2][0] = cubes[4][0]
             cubes[2][1] = cubes[4][1]
             cubes[2][2] = cubes[4][2]
@@ -53,6 +60,15 @@ def solution(loop_case, data):
             tmp.append(cubes[2][6])
             tmp.append(cubes[2][7])
             tmp.append(cubes[2][8])
+            cubes[1][0] = tmp_cubes[1][6]
+            cubes[1][1] = tmp_cubes[1][3]
+            cubes[1][2] = tmp_cubes[1][0]
+            cubes[1][3] = tmp_cubes[1][7]
+            cubes[1][4] = tmp_cubes[1][4]
+            cubes[1][5] = tmp_cubes[1][1]
+            cubes[1][6] = tmp_cubes[1][8]
+            cubes[1][7] = tmp_cubes[1][5]
+            cubes[1][8] = tmp_cubes[1][2]
             cubes[2][6] = cubes[4][6]
             cubes[2][7] = cubes[4][7]
             cubes[2][8] = cubes[4][8]
@@ -69,6 +85,15 @@ def solution(loop_case, data):
             tmp.append(cubes[2][6])
             tmp.append(cubes[2][7])
             tmp.append(cubes[2][8])
+            cubes[1][0] = tmp_cubes[1][2]
+            cubes[1][1] = tmp_cubes[1][5]
+            cubes[1][2] = tmp_cubes[1][8]
+            cubes[1][3] = tmp_cubes[1][1]
+            cubes[1][4] = tmp_cubes[1][4]
+            cubes[1][5] = tmp_cubes[1][7]
+            cubes[1][6] = tmp_cubes[1][0]
+            cubes[1][7] = tmp_cubes[1][3]
+            cubes[1][8] = tmp_cubes[1][6]
             cubes[2][6] = cubes[5][6]
             cubes[2][7] = cubes[5][7]
             cubes[2][8] = cubes[5][8]
@@ -85,6 +110,15 @@ def solution(loop_case, data):
             tmp.append(cubes[0][2])
             tmp.append(cubes[0][5])
             tmp.append(cubes[0][8])
+            cubes[2][0] = tmp_cubes[2][6]
+            cubes[2][1] = tmp_cubes[2][3]
+            cubes[2][2] = tmp_cubes[2][0]
+            cubes[2][3] = tmp_cubes[2][7]
+            cubes[2][4] = tmp_cubes[2][4]
+            cubes[2][5] = tmp_cubes[2][1]
+            cubes[2][6] = tmp_cubes[2][8]
+            cubes[2][7] = tmp_cubes[2][5]
+            cubes[2][8] = tmp_cubes[2][2]
             cubes[0][2] = cubes[4][2]
             cubes[0][5] = cubes[4][5]
             cubes[0][8] = cubes[4][8]
@@ -101,6 +135,15 @@ def solution(loop_case, data):
             tmp.append(cubes[0][2])
             tmp.append(cubes[0][5])
             tmp.append(cubes[0][8])
+            cubes[2][0] = tmp_cubes[2][2]
+            cubes[2][1] = tmp_cubes[2][5]
+            cubes[2][2] = tmp_cubes[2][8]
+            cubes[2][3] = tmp_cubes[2][1]
+            cubes[2][4] = tmp_cubes[2][4]
+            cubes[2][5] = tmp_cubes[2][7]
+            cubes[2][6] = tmp_cubes[2][0]
+            cubes[2][7] = tmp_cubes[2][3]
+            cubes[2][8] = tmp_cubes[2][6]
             cubes[0][2] = cubes[5][0]
             cubes[0][5] = cubes[5][3]
             cubes[0][8] = cubes[5][6]
@@ -117,6 +160,15 @@ def solution(loop_case, data):
             tmp.append(cubes[0][0])
             tmp.append(cubes[0][3])
             tmp.append(cubes[0][6])
+            cubes[3][0] = tmp_cubes[3][6]
+            cubes[3][1] = tmp_cubes[3][3]
+            cubes[3][2] = tmp_cubes[3][0]
+            cubes[3][3] = tmp_cubes[3][7]
+            cubes[3][4] = tmp_cubes[3][4]
+            cubes[3][5] = tmp_cubes[3][1]
+            cubes[3][6] = tmp_cubes[3][8]
+            cubes[3][7] = tmp_cubes[3][5]
+            cubes[3][8] = tmp_cubes[3][2]
             cubes[0][0] = cubes[5][2]
             cubes[0][3] = cubes[5][5]
             cubes[0][6] = cubes[5][8]
@@ -133,6 +185,15 @@ def solution(loop_case, data):
             tmp.append(cubes[0][0])
             tmp.append(cubes[0][3])
             tmp.append(cubes[0][6])
+            cubes[3][0] = tmp_cubes[3][2]
+            cubes[3][1] = tmp_cubes[3][5]
+            cubes[3][2] = tmp_cubes[3][8]
+            cubes[3][3] = tmp_cubes[3][1]
+            cubes[3][4] = tmp_cubes[3][4]
+            cubes[3][5] = tmp_cubes[3][7]
+            cubes[3][6] = tmp_cubes[3][0]
+            cubes[3][7] = tmp_cubes[3][3]
+            cubes[3][8] = tmp_cubes[3][6]
             cubes[0][0] = cubes[4][0]
             cubes[0][3] = cubes[4][3]
             cubes[0][6] = cubes[4][6]
@@ -149,6 +210,15 @@ def solution(loop_case, data):
             tmp.append(cubes[0][6])
             tmp.append(cubes[0][7])
             tmp.append(cubes[0][8])
+            cubes[4][0] = tmp_cubes[4][6]
+            cubes[4][1] = tmp_cubes[4][3]
+            cubes[4][2] = tmp_cubes[4][0]
+            cubes[4][3] = tmp_cubes[4][7]
+            cubes[4][4] = tmp_cubes[4][4]
+            cubes[4][5] = tmp_cubes[4][1]
+            cubes[4][6] = tmp_cubes[4][8]
+            cubes[4][7] = tmp_cubes[4][5]
+            cubes[4][8] = tmp_cubes[4][2]
             cubes[0][6] = cubes[3][2]
             cubes[0][7] = cubes[3][5]
             cubes[0][8] = cubes[3][8]
@@ -165,6 +235,15 @@ def solution(loop_case, data):
             tmp.append(cubes[0][6])
             tmp.append(cubes[0][7])
             tmp.append(cubes[0][8])
+            cubes[4][0] = tmp_cubes[4][2]
+            cubes[4][1] = tmp_cubes[4][5]
+            cubes[4][2] = tmp_cubes[4][8]
+            cubes[4][3] = tmp_cubes[4][1]
+            cubes[4][4] = tmp_cubes[4][4]
+            cubes[4][5] = tmp_cubes[4][7]
+            cubes[4][6] = tmp_cubes[4][0]
+            cubes[4][7] = tmp_cubes[4][3]
+            cubes[4][8] = tmp_cubes[4][6]
             cubes[0][6] = cubes[2][0]
             cubes[0][7] = cubes[2][3]
             cubes[0][8] = cubes[2][6]
@@ -181,6 +260,15 @@ def solution(loop_case, data):
             tmp.append(cubes[0][0])
             tmp.append(cubes[0][1])
             tmp.append(cubes[0][2])
+            cubes[5][0] = tmp_cubes[5][6]
+            cubes[5][1] = tmp_cubes[5][3]
+            cubes[5][2] = tmp_cubes[5][0]
+            cubes[5][3] = tmp_cubes[5][7]
+            cubes[5][4] = tmp_cubes[5][4]
+            cubes[5][5] = tmp_cubes[5][1]
+            cubes[5][6] = tmp_cubes[5][8]
+            cubes[5][7] = tmp_cubes[5][5]
+            cubes[5][8] = tmp_cubes[5][2]
             cubes[0][0] = cubes[2][2]
             cubes[0][1] = cubes[2][5]
             cubes[0][2] = cubes[2][8]
@@ -197,6 +285,15 @@ def solution(loop_case, data):
             tmp.append(cubes[0][0])
             tmp.append(cubes[0][1])
             tmp.append(cubes[0][2])
+            cubes[5][0] = tmp_cubes[5][2]
+            cubes[5][1] = tmp_cubes[5][5]
+            cubes[5][2] = tmp_cubes[5][8]
+            cubes[5][3] = tmp_cubes[5][1]
+            cubes[5][4] = tmp_cubes[5][4]
+            cubes[5][5] = tmp_cubes[5][7]
+            cubes[5][6] = tmp_cubes[5][0]
+            cubes[5][7] = tmp_cubes[5][3]
+            cubes[5][8] = tmp_cubes[5][6]
             cubes[0][0] = cubes[3][0]
             cubes[0][1] = cubes[3][3]
             cubes[0][2] = cubes[3][6]
@@ -212,12 +309,7 @@ def solution(loop_case, data):
 
 
 for _ in range(test_case):
-    solution(loop_case, data)
-    print(cubes[0][6]+cubes[0][3]+cubes[0][0])
-    print(cubes[0][7]+cubes[0][4]+cubes[0][1])
-    print(cubes[0][8]+cubes[0][5]+cubes[0][2])
-    print(cubes)
-    
+    cubes = [[0 for _ in range(9)] for _ in range(6)]
     count = 0
     for i in colors:
         for j in range(9):
@@ -225,6 +317,13 @@ for _ in range(test_case):
         count += 1
     loop_case = int(input())
     data = list(map(str, input().split()))
+    
+    solution(data)
+    print(cubes[0][6]+cubes[0][3]+cubes[0][0])
+    print(cubes[0][7]+cubes[0][4]+cubes[0][1])
+    print(cubes[0][8]+cubes[0][5]+cubes[0][2])
+
+
 
 """
 0 > 3
