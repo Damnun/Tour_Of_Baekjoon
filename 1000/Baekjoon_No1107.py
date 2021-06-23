a0b610ab269a4287b802f618f829931ed6fddbc9
이재헌 <shmom467@hanmail.net>
2021-06-23 14:08:57 +0000

    이것도 꽤나 고전했던 문제. 브루트포스로 난도 있는 문제를 푸는게 어렵다. 리모컨의 고장난 버튼을 set으로 제외해주고 단순히 +/-를 이용할 때, 사용할 수 없는 버튼을 제외하고 최적의 값을 (min)으로 구해주었다.

--- 1000/Baekjoon_No1107.py
+++ 1000/Baekjoon_No1107.py
@@ -15,3 +15,10 @@ for channel in range(1000001):
         elif len(str(channel)) - 1 == i:
             minimum = min(minimum, abs(channel - N) + len(str(channel)))
 print(minimum)
+
+"""
+이것도 꽤나 고전했던 문제.
+브루트포스로 난도 있는 문제를 푸는게 어렵다.
+리모컨의 고장난 버튼을 set으로 제외해주고
+단순히 +/-를 이용할 때, 사용할 수 없는 버튼을 제외하고 최적의 값을 (min)으로 구해주었다.
+"""
