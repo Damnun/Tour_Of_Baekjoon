023c4447eb3ca21ee974fc36bb0a8409e8521247
이재헌 <shmom467@hanmail.net>
2021-07-02 14:18:21 +0000

    DP 메모이제이션을 이용해 시간 초과 해결 11659번과 마찬가지의 해법으로 풀이하였따.

--- 10000/11000/Baekjoon_No11441.py
+++ 10000/11000/Baekjoon_No11441.py
@@ -1,6 +1,7 @@
- import sys
-N, M = map(int, sys.stdin.readline().split())
+import sys
+N = int(sys.stdin.readline().strip())
 data = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline().strip())
 dp = [data[0]]
 
 for i in range(1, N):
@@ -12,9 +13,3 @@ for _ in range(M):
         print(dp[end - 1])
     else:
         print(dp[end - 1] - dp[start - 2])
-
-"""
-DP를 이용해 시간초과 해결
-미리 data 값을 dp에 메모이제이션 한 후
-start 값에 따라 미리 구해둔 값을 빼주기만 하였다.
-"""
