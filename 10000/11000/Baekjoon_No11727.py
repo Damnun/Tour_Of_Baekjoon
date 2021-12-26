8aa757aff0201b5f564ebdf92a9a3651032ecd36
Universe_lee <72370701+Damnun@users.noreply.github.com>
2021-12-26 23:39:40 +0900

    Create Baekjoon_No11727.py

---
+++ 10000/11000/Baekjoon_No11727.py
@@ -0,0 +1,13 @@
+dp = [0 for _ in range(1001)]
+dp[1] = 1
+dp[2] = 3
+dp[3] = 5
+n = int(input())
+
+if n < 4:
+    print(dp[n])
+    exit()
+
+for i in range(4, n + 1):
+    dp[i] = dp[i - 1] + (dp[i - 2] * 2)
+print(dp[n] % 10007)
