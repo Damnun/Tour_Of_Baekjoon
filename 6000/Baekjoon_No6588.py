66b5d12cb402fad46b4e9caaf0d4e6e06ed7665c
Universe_lee <72370701+Damnun@users.noreply.github.com>
2021-12-28 13:34:52 +0900

    Create Baekjoon_No6588.py

---
+++ 6000/Baekjoon_No6588.py
@@ -0,0 +1,25 @@
+MAX = 1000000
+check = [0] * (MAX + 1)
+check[0] = check[1] = True
+prime = []
+
+for i in range(2, MAX + 1):
+    if not check[i]:
+        prime.append(i)
+        j = i + i
+        while j <= MAX:
+            check[j] = True
+            j += i
+prime = prime[1:]
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    for p in prime:
+        if (n - p) < 0:
+            print("Goldbach's conjecture is wrong.")
+            break
+        elif not check[n - p]:
+            print(f'{n} = {p} + {n - p}')
+            break
