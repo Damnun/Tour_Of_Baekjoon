f26189609a7c3de708c7ac9d48afb76065a8f588
Universe_lee <72370701+Damnun@users.noreply.github.com>
2021-12-28 23:01:38 +0900

    Create Baekjoon_No2309.py

---
+++ 2000/Baekjoon_No2309.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+data = []
+for _ in range(9):
+    data.append(int(input()))
+
+for i in combinations(data, 7):
+    if sum(i) == 100:
+        result = list(i)
+        break
+
+result.sort()
+for i in result:
+    print(i)
