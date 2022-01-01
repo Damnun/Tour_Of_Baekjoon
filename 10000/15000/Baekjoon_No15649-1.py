e443caa8f2cb1a27e2cbfe4e3813a5aad720ed35
Universe_lee <72370701+Damnun@users.noreply.github.com>
2022-01-01 23:36:47 +0900

    Create Baekjoon_No15649-1.py
    재귀함수를 이용하여 N과 M을 풀이하였다.
    재귀를 사용한 건 어떤 위치에 올 수 있는 수를 결정하기 위함인데
    반복문을 사용하지 않은 이유는 
    방법은 같고 위치만(INDEX)만 다르기에 재귀를 사용한 것 이다.

---
+++ 10000/15000/Baekjoon_No15649-1.py
@@ -0,0 +1,21 @@
+# 1부터 n까지 자연수 중 중복 없이 m개를 고른 수열 전체
+# 1 <= m < n <= 8
+import sys
+n, m = map(int, input().split())
+data = [0] * m
+c = [False] * (n + 1)
+
+def go(index, n, m):
+    if index == m:
+        print(*data)
+        return
+
+    for i in range(1, n + 1):
+        if c[i]:
+            continue
+        c[i] = True
+        data[index] = i
+        go(index + 1, n, m)
+        c[i] = False
+
+go(0, n, m)
