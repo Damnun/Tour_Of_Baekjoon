d1b8840d19ad309667841619fd3b21fbfacb1667
Universe_lee <72370701+Damnun@users.noreply.github.com>
2022-01-02 00:42:09 +0900

    Create Baekjoon_No15656.py

---
+++ 10000/15000/Baekjoon_No15656.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+data = [0] * m
+ns = list(map(int, input().split()))
+ns.sort()
+
+def go(index, start, n, m):
+    if index == m:
+        print(*data)
+        return
+
+    for i in range(start, n):
+        data[index] = ns[i]
+        go(index + 1, 0, n, m)
+
+go(0, 0, n, m)
