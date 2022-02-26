3786a49bd88c01413c06af7fbb9e1af72f9afe5b
Universe_lee <72370701+Damnun@users.noreply.github.com>
2022-02-26 23:22:45 +0900

    Update Baekjoon_No4690.py

--- 4000/Baekjoon_No4690.py
+++ 4000/Baekjoon_No4690.py
@@ -1,8 +1,8 @@
-for a in range(6,101):
-    for b in range(2,101):
-        for c in range(b+1,101):
-            for d in range(c+1,101):
-                if a**3 == b**3 + c**3 + d**3:
-                    print(f"Cube = {a}, Triple = ({b},{c},{d})")
-                if a**3 < b**3 + c**3 + d**3:
+for i in range(6, 101):
+    for j in range(2, 101):
+        for k in range(j + 1, 101):
+            for l in range(k + 1, 101):
+                if i ** 3 == (j ** 3 + k ** 3 + l ** 3):
+                    print(f"Cube = {i}, Triple = ({j}, {k}, {l})")
+                if i ** 3 < (j ** 3 + k ** 3 + l ** 3):
                     break
