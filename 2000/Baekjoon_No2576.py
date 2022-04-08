976b86116e3c894627285af5817bdcdd85244fd3
Universe_lee <72370701+Damnun@users.noreply.github.com>
2022-04-08 13:49:22 +0900

    Create Baekjoon_No2576.py
    math

---
+++ 2000/Baekjoon_No2576.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+ans = []
+for i in range(7):
+    a = int(input())
+    if a % 2 != 0:
+        ans.append(a)
+if ans:
+    print(sum(ans))
+    print(min(ans))
+else:
+    print(-1)
