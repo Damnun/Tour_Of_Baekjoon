7ac44625f9e46fd2d38d3b21238e2e6e70eaef93
Universe_lee <72370701+Damnun@users.noreply.github.com>
2022-04-12 21:22:28 +0900

    Create Baekjoon_No2501.py
    Brute Force

---
+++ 2000/Baekjoon_No2501.py
@@ -0,0 +1,12 @@
+ans = []
+n, k = map(int, input().split())
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        ans.append(i)
+
+if k > len(ans):
+    print(0)
+    
+else:
+    print(ans[k - 1])
