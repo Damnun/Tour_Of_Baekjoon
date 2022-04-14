744fb8a911a85b6a82c3303f11391c35c0298081
Universe_lee <72370701+Damnun@users.noreply.github.com>
2022-04-15 02:15:05 +0900

    Create Baekjoon_No2490.py
    implemental

---
+++ 2000/Baekjoon_No2490.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+a = [sum(map(int, input().split())) for _ in range(3)]
+ 
+for i in a:
+    if i == 3:
+        print('A')
+    elif i == 2:
+        print('B')
+    elif i == 1:
+        print('C')
+    elif not i:
+        print('D')
+    elif i == 4:
+        print('E')
