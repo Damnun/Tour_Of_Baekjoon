6535f58d1a6e583678314d1d2309e3fe6d33e734
Universe_lee <72370701+Damnun@users.noreply.github.com>
2022-04-15 22:49:08 +0900

    Create Baekjoon_No2729.py
    math

---
+++ 2000/Baekjoon_No2729.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+for _ in range(n):
+    a, b = input().split(" ")
+    a = int(a, 2)
+    b = int(b, 2)
+    print(bin(a + b).replace("0b", ""))
