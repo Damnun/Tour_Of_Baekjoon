6e18333150e2106db5bd940c5e98bc1a91e33ba6
Universe_lee <72370701+Damnun@users.noreply.github.com>
2022-06-14 13:55:13 +0900

    Create Baekjoon_No10886.py

---
+++ 10000/Baekjoon_No10886.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = 0
+
+for _ in range(n):
+    if int(input()) == 1:
+        a += 1
+
+if a > n // 2:
+    print("Junhee is cute!")
+    
+else:
+    print("Junhee is not cute!")
