4e8bd8b1b8aa20f7ccb271a437b3b47f6ebe70d8
Universe_lee <72370701+Damnun@users.noreply.github.com>
2024-01-23 22:31:33 +0900

    Create Baekjoon_No30987.py

---
+++ 30000/Baekjoon_No30987.py
@@ -0,0 +1,9 @@
+x1, x2 = map(int, input().split())
+a, b, c, d, e = map(int, input().split())
+
+
+def integral(a, b, c, d, e, f):
+    return ((a * (f ** 3)) // 3) + ((b * f ** 2) // 2) + (c * f) - (((d * f ** 2) // 2) + e * f)
+
+
+print(integral(a, b, c, d, e, x2) - integral(a, b, c, d, e, x1))
