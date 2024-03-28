a531478c21dc65a828ada41d5f83223565847709
Universe_lee <72370701+Damnun@users.noreply.github.com>
2024-03-29 01:54:01 +0900

    Create Baekjoon_No2562.java

---
+++ 2000/Baekjoon_No2562.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int maxValue = 0;
+        int maxIdx = -1;
+
+        for (int i = 0; i < 9; i++) {
+            int cur = Integer.parseInt(br.readLine());
+            if (cur > maxValue) {
+                maxValue = cur;
+                maxIdx = i + 1;
+            }
+        }
+
+        System.out.println(maxValue);
+        System.out.println(maxIdx);
+    }
+}
