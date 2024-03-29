30bd980255d3bb0ae7da340ab85cd32763039abb
Universe_lee <72370701+Damnun@users.noreply.github.com>
2024-03-29 17:23:05 +0900

    [S2] Binary Search

---
+++ 1000/Baekjoon_No1654.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b1654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[k];
+        long max = 0;
+
+        for (int i = 0; i < k; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        max++;
+        long min = 0, mid = 0;
+        while (min < max) {
+            mid = (max + min) / 2;
+            long count = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                count += (arr[i] / mid);
+            }
+
+            if (count < n) {
+                max = mid;
+            }
+            else {
+                min = mid + 1;
+            }
+        }
+
+        System.out.println(min - 1);
+    }
+}
