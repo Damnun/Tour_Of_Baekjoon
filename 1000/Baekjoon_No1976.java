449252f91557527c0c83a4c6ec55f96c687cc1f9
Universe_lee <72370701+Damnun@users.noreply.github.com>
2024-04-11 02:27:53 +0900

    [FIX] : array Range n/m -< n/n

--- 1000/Baekjoon_No1976.java
+++ 1000/Baekjoon_No1976.java
@@ -7,7 +7,7 @@ public class b1976 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[][] dosi = new int[n + 1][m + 1];
+        int[][] dosi = new int[n + 1][n + 1];
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
