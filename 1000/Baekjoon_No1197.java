02a8a5be23fe977fb60b011bfa3f1d62530d069b
Universe_lee <72370701+Damnun@users.noreply.github.com>
2024-04-12 16:14:02 +0900

    [G4] Minimum Spanning Tree

---
+++ 1000/Baekjoon_No1197.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class b1197 {
+
+    static int[] parent;
+    static PriorityQueue<Edge> queue;
+
+    public static class Edge implements Comparable<Edge> {
+        int s;
+        int e;
+        int v;
+
+        public Edge(int s, int e, int v) {
+            this.s = s;
+            this.e = e;
+            this.v = v;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.v - o.v;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        queue = new PriorityQueue<>();
+        parent = new int[n + 1];
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            queue.add(new Edge(s, e, v));
+        }
+
+        int useEdge = 0;
+        int result = 0;
+
+        while (useEdge < n - 1) {
+            Edge now = queue.poll();
+            if (find(now.s) != find(now.e)) {
+                union(now.s, now.e);
+                result = result + now.v;
+                useEdge++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    public static int find(int x) {
+        if (x == parent[x])
+            return x;
+        else return parent[x] = find(parent[x]);
+    }
+}
