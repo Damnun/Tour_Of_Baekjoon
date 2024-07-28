14283441599961b1bbb7e868c7dcb76f40b42ac8
Universe_lee <72370701+Damnun@users.noreply.github.com>
2024-07-28 23:40:02 +0900

    [B5] 입출력

---
+++ 10000/Baekjoon_No10926.java
@@ -0,0 +1,10 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class b10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(new InputStreamReader(System.in));
+        String id = sc.nextLine();
+        System.out.println(id + "??!");
+    }
+}
