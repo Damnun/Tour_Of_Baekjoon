e7074158b2916e0628cc7abc00ff7951b5a86df3
Universe_lee <72370701+Damnun@users.noreply.github.com>
2024-07-30 22:09:21 +0900

    [B2] 배열

---
+++ 10000/Baekjoon_No10811.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class b10811 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int s = sc.nextInt() - 1;
+            int e = sc.nextInt() - 1;
+
+            while (s < e) {
+                int tmp = arr[s];
+                arr[s++] = arr[e];
+                arr[e--] = tmp;
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
